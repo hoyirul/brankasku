daeeee0a65a276e149a4c8b997a261873182fde0
hoyirul <irulgraspama621@gmail.com>
2023-04-02 01:23:33 +0700

    FIx

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Before Login
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 
+// After Login
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 
