a84dd45aec2d147779c4f39c3f37d12953cca19f
hoyirul <irulgraspama621@gmail.com>
2023-04-07 19:50:37 +0700

    Fix

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/register', [AuthController::class, 'register']);
 
-// After Login
+// After Logins
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/auth/logout', [AuthController::class, 'logout']);
 
